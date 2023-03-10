1f7f947b74e950d01f784ce5abbed52475db6287
Laurent Bourget <Laurent.Bourget@cirquedusoleil.com>
2023-03-10 12:31:01 -0500

    feat: change socket server url

--- plugins/socket.client.js
+++ plugins/socket.client.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
-const socket = io("http://localhost:3000");
+const socket = io("https://nuxt-websocket.netlify.app");
 
 export default ({}, inject) => {
   inject("socket", socket);
